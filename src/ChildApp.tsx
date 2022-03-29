3a1d0b71f9226961f16d4ae0fbdf033d61f8b49f
“iwataTMC” <abcwwwxyz888@gmail.com>
2022-03-29 23:34:24 +0900

    Add build folder

---
+++ src/ChildApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function ChildApp() {
+  return (
+    <div className="ChildApp">
+      <main>
+        <p>2nd Compornent</p>
+      </main>
+    </div>
+  );
+}
+
+export default ChildApp;
