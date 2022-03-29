01680502d6bcc426a3b08b606455c97e2e52d2c0
“iwataTMC” <abcwwwxyz888@gmail.com>
2022-03-29 23:33:40 +0900

    Add build folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ChildApp from './ChildApp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ChildApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
